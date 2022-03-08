96c22836135dabfc254334aa7b16ebe359429d1d
olamide142 <olowofesovictor@gmail.com>
2022-03-08 00:08:28 +0000

    i hate javascript!

--- handwritting_ai.py
+++ handwritting_ai.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from PIL import Image, ImageDraw
 
 
@@ -36,9 +36,14 @@ TEMPLATE = \
         <script src="//cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.js" integrity="sha256-yr4fRk/GU1ehYJPAs8P4JlTgu0Hdsp4ZKrx8bDEDC3I=" crossorigin="anonymous"></script>
         <script>
             
+            var xhr = new XMLHttpRequest();
+            
             var Points = [];
             var WIDTH = screen.width;
             var HEIGHT =  screen.height;
+            var boundingBox = [];
+            var showBoundingBox = false;
+            var res;
 
             function setup() {
                 createCanvas(WIDTH, HEIGHT);
@@ -49,21 +54,41 @@ TEMPLATE = \
 
             function draw() {
                 stroke(0);
+
                 coordinate = [mouseX, mouseY, pmouseX, pmouseY];
                 if (mouseIsPressed === true) {
                     Points.push(coordinate)
                     line.apply(null, coordinate);
                 }
+
+                if (showBoundingBox === true){
+                    for (let point_xy in Points){
+                        line.apply(null, Points[point_xy]);
+                    }
+                    fill(120);
+                    setLineDash([5, 5]); //create the dashed line pattern here
+                    rect.apply(null, boundingBox);
+                    setLineDash([0, 0]); //create the dashed line pattern here
+
+                }
+            }
+
+
+            function setLineDash(list) {
+                drawingContext.setLineDash(list);
             }
 
             function giveMeText() {
-                var xhr = new XMLHttpRequest();
                 xhr.open("POST", '/getHandwritting', true);
 
                 //Send the proper header information along with the request
                 xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
                 xhr.send(JSON.stringify({points : Points, dimension : [WIDTH, HEIGHT]}));
-                Points = [];
+                res = JSON.parse(xhr.responseText);
+                console.log(res)
+                showBoundingBox = true;
+                redraw(1);
+                showBoundingBox = false;
             }
         </script>
 
@@ -84,11 +109,40 @@ TEMPLATE = \
 """
 
 
+
+class BoundingBox:
+    """ To handle cropping """
+
+    def __init__(self, file):
+        self.points = map(
+                        lambda x: map(float, x.split(',')), 
+                        open(file, 'r').readlines()
+                    )
+        self.minX = float("inf")
+        self.minY = float("inf")
+        self.maxX = -float("inf")
+        self.maxY = -float("inf")
+
+
+    def getBoundingBox(self):
+        for point in self.points:
+            x1,y1,x2,y2 = point
+            self.minX = min([self.minX, x1, x2])
+            self.minY = min([self.minY, y1, y2])
+            self.maxX = max([self.maxX, x1, x2])
+            self.maxY = max([self.maxY, y1, y2])
+
+        return {'topLeft': (self.minX, self.maxY),
+                'topRight': (self.maxX, self.maxY),
+                'bottomRight': (self.maxX, self.minY),
+                'bottomLeft': (self.minX, self.minY)
+            }
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-
     return TEMPLATE
 
 
@@ -97,18 +151,19 @@ def getPoints():
     os.remove('pointsXY.txt')
     f = open('pointsXY.txt', 'a')
 
-    if request.method == 'POST':
-        last = None
+    last = None
 
-        for point in request.json.get('points'):
-            if point != last:
-                last = point
-                f.write(",".join(map(str, point)) + '\n')
+    for point in request.json.get('points'):
+        if point != last:
+            last = point
+            f.write(",".join(map(str, point)) + '\n')
 
-        f.close()
-        createImage(*request.json.get('dimension'))
-
-    return TEMPLATE
+    f.close()
+    
+    # Create an image of the sketch
+    createImage(*request.json.get('dimension'))
+    # Get bounding box of the sketch
+    return jsonify(BoundingBox('pointsXY.txt').getBoundingBox())
 
 
 def createImage(width, height):
@@ -121,7 +176,6 @@ def createImage(width, height):
         for i in open('pointsXY.txt').readlines():
             x1,y1,x2,y2 = map(float, i.split(","))
             draw.line(((x1, y1), (x2, y2)), fill=225//2, width=2)
-        # draw.line((10, im.size[1], im.size[0], 0), fill=128)
 
         im.save("image.png", "PNG")
 
